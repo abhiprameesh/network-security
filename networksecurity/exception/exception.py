32e2b19f7a6284dc31ba5d3962965bad5edaa4c2
Abhinand Prameesh <abhiprameesh@gmail.com>
2026-03-22 17:49:55 +0530

    logger done

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,18 @@
+import sys
+
+class NetworkSecurityException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+
+        self.lineno=exc_tb.tb_lineno
+        self.filename=exc_tb.tb_frame.f_code.co_filename
+    def __str__(self): 
+        return "Error occurred in python script name [{0}] at line number [{1}] error message [{2}]".format(
+        self.filename,self.lineno,self.error_message)
+    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
